906f924b8bf6477c4445adf2778630dbc1c1690c
roblig2 <rob.ligman@gmail.com>
2025-03-27 20:53:28 +0100

    add product quantity

--- zadanie3.1.1.py
+++ zadanie3.1.1.py
@@ -9,3 +9,6 @@ for shop, products in shopping_list.items():
     shop = shop.capitalize()
     products = [product.capitalize() for product in products]
     print(f"Idę do {shop}, kupuję tu następujące rzeczy: {products}.")
+
+products_qty = sum(len(produkty) for produkty in shopping_list.values())
+print(f"W sumie kupuję {products_qty} produktów.")
